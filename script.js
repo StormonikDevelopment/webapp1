7f2836347915548718130c148c32666b28130c44
KateoDevelopment <73410312+StormonikDevelopment@users.noreply.github.com>
2022-01-19 11:24:17 -0700

    ook

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-// edit
+function Buttonclick() {
+
+document.createElement('div');
+document.getElementById('div');
+document.onclick.createElement('null');
+
+
+}
+
+document.getElementById(b1).onclick.Buttonclick;
